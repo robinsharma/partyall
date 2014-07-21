f06353d77aa11a2572235d37d84c7597d1d46cf9
Robin Sharma <sharms115@gmail.com>
2014-07-20 20:49:54 -0400

    Login angular logic

--- app/js/app.js
+++ app/js/app.js
@@ -13,6 +13,7 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
   $routeProvider.when('/login/host', {templateUrl: 'partials/login-host.html', controller: 'LoginHostCtrl'});
   $routeProvider.when('/login/guest', {templateUrl: 'partials/login-guest.html', controller: 'LoginGuestCtrl'});
+  $routeProvider.when('/login/success', {templateUrl: 'partials/login-success.html', controller: 'LoginSuccessCtrl'});
   // $routeProvider.when('/party/:userType/:partyId', {templateUrl: 'partials/party.html', controller: 'PartyCtrl'});
   // $routeProvider.when('/party/:userType/:partyId/search', {templateUrl: 'partials/search.html', controller: 'SearchCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,12 +3,28 @@
 /* Controllers */
 
 angular.module('partyAll.controllers', [])
-  .controller('HomeCtrl', ['$scope', function($scope) {
+  .controller('HomeCtrl', ['$scope', 
+    function($scope) {
 
   }])
-  .controller('LoginHostCtrl', ['$scope', function($scope) {
+  .controller('LoginHostCtrl', ['$scope', '$http', 
+    function($scope, $http) {
+        $scope.formData = {};
+
+  }])  
+  .controller('CreatePartyCtrl', ['$scope', '$http', 
+    function($scope, $http) {
+        $scope.formData = {};
+        this.createParty = function() {
+            console.log('TODO make request');
+        };
 
   }])
-  .controller('LoginGuestCtrl', ['$scope', function($scope) {
+  .controller('LoginGuestCtrl', ['$scope', 
+    function($scope) {
+
+  }])
+  .controller('LoginSuccessCtrl', ['$scope', 
+    function($scope) {
 
   }]);
