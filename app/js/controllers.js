074a9befc3d64fc855e5793b4fa354aeda8835d9
jmuia <joseph.b.muia@gmail.com>
2014-07-28 00:33:59 -0400

    basic now playing interface for host -- using soundcloud api

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -64,8 +64,90 @@ angular.module('partyAll.controllers', [])
     function($scope, PartyService, Session, USER_TYPES){
       $scope.party = PartyService.party;
       $scope.user = { type: Session.userType, id: Session.userId };
-      $scope.isHost = function() {
-        return Session.userType === USER_TYPES.host;
-      }
+
       
-  }]);
+      $scope.isHost = function() {
+        // return true;
+        return Session.userType === USER_TYPES.host;
+      };
+
+      $scope.songPlaying = function() {
+        return true;
+        // return $scope.party.now_playing;
+      };
+      
+  }])
+
+.controller('PlayerCtrl', ['$scope', '$http', '$document', '$interval', 
+  function($scope, $http, $document, $interval) {
+    var soundcloudBaseUrl = 'http://api.soundcloud.com',
+        soundcloudClientIdParam = '?client_id=11c11021d4d8721cf1970667907f45d6';
+
+    var getPlaylist = function(callback) {
+      $http
+      .get(soundcloudBaseUrl + '/tracks' + soundcloudClientIdParam + '&q=problem')
+      .success(function (tracks) {
+        console.log(tracks);
+        queue = tracks;
+        callback();
+      });
+
+    };
+
+    $scope.playerControl = function () {
+      if (!$scope.isPlaying) {
+        if (audio.getAttribute('src') === null) {
+          setPlayer();
+        }
+        audio.play();
+        $scope.isPlaying = true;
+        timer = $interval(updateTime, 1000);
+      } else {
+        audio.pause();
+        $scope.isPlaying = false;
+        $interval.cancel(timer);
+      }
+    };
+
+    $scope.nextSong = function () {
+      $interval.cancel(timer);
+      currentSongIdx++;
+      setPlayer();
+      audio.play();
+      $scope.isPlaying = true;
+      timer = $interval(updateTime, 1000);
+    };
+
+    var setPlayer = function () {
+      $scope.timeWidth = "0%";
+      $scope.artwork = queue[currentSongIdx].artwork_url;
+      $scope.title = queue[currentSongIdx].title;
+      $scope.artist = queue[currentSongIdx].user.username;
+      audio.src = queue[currentSongIdx].stream_url + '?client_id=11c11021d4d8721cf1970667907f45d6';
+    };
+
+    var updateTime = function() {
+      $scope.timeWidth = parseInt(audio.currentTime * 100 / audio.duration) + "%";
+    };
+
+    $scope.artwork = null;
+    $scope.title = null;
+    $scope.artist = null;
+    $scope.isPlaying = false;
+    $scope.timeWidth = null;
+    var currentSongIdx = 0;
+    var queue = [];
+    var song = {};
+    var audio = $document[0].querySelector('#player');
+    var timer;
+
+    //init
+    getPlaylist(function() {
+      $scope.playerControl();
+    });
+
+    audio.addEventListener('ended', function () {
+      $scope.$apply($scope.nextSong());
+    });
+
+}]);
